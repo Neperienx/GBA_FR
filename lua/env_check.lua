1cd225d93ee4e25ea2dffe6754cd08929c067b6e
Neperien <nicolas.leteinturier2.0@gmail.com>
2025-11-05 10:56:33 +0100

    start over

--- lua/env_check.lua
+++
@@ -1,9 +0,0 @@
--- env_check.lua
-console.clear()
-console.log("Lua: ".._VERSION)
-console.log("Has 'comm'? "..tostring(comm ~= nil))
-if comm then
-  for k,v in pairs(comm) do
-    if k:match("^socket") then console.log("comm."..k.." = "..type(v)) end
-  end
-end
